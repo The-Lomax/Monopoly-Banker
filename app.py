9b232fbd0b391a4d988c3568b0ae4679dae59211
The-Lomax <kryniubest@gmail.com>
2022-02-09 19:33:20 +0800

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from libs.game_window import GameWindow
 from libs.game import Game
+import requests
 
 if __name__ == "__main__":
     newGame = Game()
--- libs/game.py
+++ libs/game.py
@@ -1,29 +1,38 @@
-import sqlite3
 import json
-import os
+import requests
 from tkinter import messagebox
-from libs.player import Player
-from libs.location import Location
+from libs.gameData import housesInfo, locationsInfo, poiInfo
 from libs.house import House
+from libs.location import Location
+from libs.player import Player
 from libs.poi import POI
-from libs.gameData import locationsInfo, housesInfo, poiInfo
 
 
 class Game:
     def __init__(self):
-        self.dbFile = "assets/game.sqlite"
         self.mainWindow = None
+        self.apiRef = 'https://3s9l7asy39.execute-api.ap-southeast-1.amazonaws.com/test/'
+        self.apiKey = ""
         self.pCount = 0
         self.players = {}
         self.locations = {}
         self.houses = {}
         self.poi = {}
+        
+        with open("C:\\Users\\Chris\\Documents\\awsapikey.txt") as mf:
+            self.apiKey += mf.read()
+        
+        self.head = {
+            "x-api-key": self.apiKey
+        }
 
-        # Check if database exists and create if does not exist
-        if not os.path.isfile(self.dbFile):
-            self.buildDb()
-        else:  # Database exists, proceed to load game data
-            self.loadData()
+        self.loadData()
+    
+    def loadData(self):
+        self.loadPlayersFromWeb()
+        # self.loadLocationsFromWeb()
+        # self.loadHousesFromWeb()
+        # self.loadPOIFromWeb()
 
     def addPlayer(self, pId: int, pName: str, pBal: int, pBkrupt: bool) -> None:
         self.players[pName] = Player(pId, pName, pBal, pBkrupt)
@@ -34,12 +43,6 @@ class Game:
                 f"VALUES ({pId}, '{pName}', {pBal}, {pBkrupt})"
         self.dbExecute(query, False)
 
-    def loadData(self):
-        self.loadPlayersFromDB()
-        self.loadLocationsFromDB()
-        self.loadHousesFromDB()
-        self.loadPOIFromDB()
-
     def addLocation(self, lId, name, buyPrice, rent, mortgage, buildPrice, buildings, rentSplits, rentDiscounts, status, ownerId) -> None:
         self.locations[name] = Location(lId, name, buyPrice, json.loads(rent), mortgage, buildPrice, buildings, json.loads(rentSplits), json.loads(rentDiscounts), status, ownerId)
 
@@ -49,7 +52,7 @@ class Game:
     def addPOI(self, lId, name, buyPrice, rent, mortgage, status, ownerId):
         self.poi[name] = POI(lId, name, buyPrice, json.loads(rent), mortgage, status, ownerId)
 
-    def loadHousesFromDB(self):
+    def loadHousesFromWeb(self):
         query = "SELECT * FROM houses"
         data = self.dbExecute(query, True)
         self.updateHousesList(data)
@@ -60,7 +63,7 @@ class Game:
             lId, name, buyPrice, rent, mortgage, status, ownerId = el
             self.addHouse(lId, name, buyPrice, rent, mortgage, status, ownerId)
 
-    def loadPOIFromDB(self):
+    def loadPOIFromWeb(self):
         query = "SELECT * FROM poi"
         data = self.dbExecute(query, True)
         self.updatePOIList(data)
@@ -71,7 +74,7 @@ class Game:
             lId, name, buyPrice, rent, mortgage, status, ownerId = el
             self.addPOI(lId, name, buyPrice, rent, mortgage, status, ownerId)
     
-    def loadLocationsFromDB(self):
+    def loadLocationsFromWeb(self):
         query = "SELECT * FROM locations"
         data = self.dbExecute(query, True)
         self.updateLocationsList(data)
@@ -82,10 +85,14 @@ class Game:
             lId, name, buyPrice, rent, mortgage, buildPrice, buildings, rentSplits, rentDiscounts, status, ownerId = el
             self.addLocation(lId, name, buyPrice, rent, mortgage, buildPrice, buildings, rentSplits, rentDiscounts, status, ownerId)
     
-    def loadPlayersFromDB(self):
-        query = "SELECT * FROM players"
-        data = self.dbExecute(query, True)
-        self.updatePlayersList(data)
+    def loadPlayersFromWeb(self):
+        data = requests.request(
+            "GET",
+            self.buildAPIURL("players")
+        )
+        data = data.json()
+        for el in data:
+            self.addPlayer(int(el["pId"]["N"]), str(el["pName"]), bal, bkrpt)
 
     def updatePlayersList(self, list):
         self.players = {}
@@ -94,6 +101,17 @@ class Game:
             pId, pName, pBal, pBkrpt = list[i]
             self.addPlayer(pId, pName, pBal, pBkrpt)
     
+    def buildAPIURL(self, param):
+        return "".join([self.apiRef, param, "/"])
+    
+    def deletePlayer(self, pId):
+        data = requests.request(
+            'DELETE', 
+            "".join([self.buildAPIURL("players"), str(pId)]),
+            headers=self.head
+        )
+        return data
+    
     def center(self, container, *app_size: int):
         return f"{app_size[0]}x{app_size[1]}+" + \
                f"{container.winfo_screenwidth() // 2 - app_size[0] // 2}+" + \
@@ -101,118 +119,34 @@ class Game:
 
     def resetGameState(self):
         if messagebox.askyesno("Warning", "This will reset the game to default settings. Are you sure?", parent=self.mainWindow):
-            db = sqlite3.connect(self.dbFile)
-            cursor = db.cursor()
+            
+            for el in locationsInfo.values():
+                data = requests.request(
+                    'PUT', 
+                    self.apiRef,
+                    headers=self.head,
+                    json=el
+                )
+            
+            for el in housesInfo.values():
+                data = requests.request(
+                    'PUT', 
+                    'https://3s9l7asy39.execute-api.ap-southeast-1.amazonaws.com/test/locations',
+                    headers=self.head,
+                    json=el
+                )
 
-            query = f"DELETE FROM players WHERE id>0"
-            cursor.execute(query)
-
-            query = f"UPDATE houses SET status='free', ownerId=0"
-            cursor.execute(query)
-
-            query = f"UPDATE poi SET status='free', ownerId=0"
-            cursor.execute(query)
-
-            query = f"UPDATE locations SET buildings=0, rentSplits='{json.dumps(dict())}', rentDiscounts='{json.dumps(dict())}', status='free', ownerId=0"
-            cursor.execute(query)
-
-            db.commit()
-            db.close()
+            for el in poiInfo.values():
+                data = requests.request(
+                    'PUT', 
+                    'https://3s9l7asy39.execute-api.ap-southeast-1.amazonaws.com/test/locations',
+                    headers=self.head,
+                    json=el
+                )
+            
+            for el in self.players.values():
+                self.deletePlayer(el['pId'])
 
             self.loadData()
             self.mainWindow.showModule(self.mainWindow.playersFrame)
             self.mainWindow.playersFrame.loadPlayers()
-
-    def buildDb(self):
-        db = sqlite3.connect(self.dbFile)
-        cursor = db.cursor()
-
-        query = f"CREATE TABLE IF NOT EXISTS players (" \
-                f"id INTEGER, " \
-                f"name TEXT, " \
-                f"balance INTEGER, " \
-                f"bankrupt INTEGER);"
-        cursor.execute(query)
-
-        query = f"INSERT INTO players (id, name, balance, bankrupt) " \
-                f"VALUES (0, 'Bank', 0, 0);"
-        cursor.execute(query)
-
-        query = f"CREATE TABLE IF NOT EXISTS locations (" \
-                f"id INTEGER, " \
-                f"name TEXT, " \
-                f"buyPrice INTEGER, " \
-                f"rent TEXT, " \
-                f"mortgage INTEGER, " \
-                f"buildPrice INTEGER, " \
-                f"buildings INTEGER, " \
-                f"rentSplits TEXT, " \
-                f"rentDiscounts TEXT, " \
-                f"status TEXT, " \
-                f"ownerId INTEGER);"
-        cursor.execute(query)
-
-        for loc in locationsInfo.values():
-            query = f"INSERT INTO locations (id, name, buyPrice, rent, mortgage, buildPrice, buildings, rentSplits, rentDiscounts, status, ownerId" \
-                    f") VALUES (" \
-                    f"{loc['id']},'{loc['name']}',{loc['buyPrice']}, '{json.dumps(loc['rent'])}', {loc['mortgage']}, {loc['buildPrice']}, " \
-                    f"{loc['buildings']}, '{json.dumps(loc['rentSplits'])}', '{json.dumps(loc['rentDiscounts'])}', '{loc['status']}', {loc['ownerId']});"
-            cursor.execute(query)
-
-        query = f"CREATE TABLE IF NOT EXISTS houses (" \
-                f"id INTEGER NOT NULL, " \
-                f"name TEXT, " \
-                f"buyPrice INTEGER, " \
-                f"rent TEXT, " \
-                f"mortgage INTEGER, " \
-                f"status TEXT, " \
-                f"ownerId INTEGER);"
-        cursor.execute(query)
-
-        for house in housesInfo.values():
-            query = f"INSERT INTO houses (" \
-                    f"id, name, buyPrice, rent, mortgage, status, ownerId" \
-                    f") VALUES (" \
-                    f"{house['id']},'{house['name']}',{house['buyPrice']}, '{json.dumps(house['rent'])}', {house['mortgage']}, '{house['status']}', {house['ownerId']});"
-            cursor.execute(query)
-
-        query = f"CREATE TABLE IF NOT EXISTS poi (" \
-                f"id INTEGER NOT NULL, " \
-                f"name TEXT, " \
-                f"buyPrice INTEGER, " \
-                f"rent TEXT, " \
-                f"mortgage INTEGER, " \
-                f"status TEXT, " \
-                f"ownerId INTEGER);"
-        cursor.execute(query)
-
-        for poi in poiInfo.values():
-            query = f"INSERT INTO poi (" \
-                    f"id, name, buyPrice, rent, mortgage, status, ownerId" \
-                    f") VALUES (" \
-                    f"{poi['id']},'{poi['name']}',{poi['buyPrice']}, '{json.dumps(poi['rent'])}', {poi['mortgage']}, '{poi['status']}', {poi['ownerId']});"
-            cursor.execute(query)
-
-        db.commit()
-        db.close()
-
-    def dbExecute(self, query: str, ret: bool) -> list or None:
-        # Execute query
-        db = sqlite3.connect(self.dbFile)
-        cursor = db.cursor()
-        cursor.execute(query)
-
-        # Return data if required by user
-        if ret:
-            data = cursor.fetchall()
-
-            # Save the database
-            db.commit()
-
-            # Close connection
-            db.close()
-
-            return data
-        else:
-            db.commit()
-            db.close()
